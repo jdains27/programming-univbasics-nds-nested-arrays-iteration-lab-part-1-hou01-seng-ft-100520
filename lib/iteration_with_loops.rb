724fab5dc505d4dca3f281768253bafc035df8fd
Jordan Dains <dains.jordan2@gmail.com>
2020-09-14 19:46:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,12 @@
-src = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
+aoa = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
  
- def find_even_values(src)
+ def find_even_values(aoa)
   row_index = 0
-  while row_index < src.count do
+  while row_index < aoa.count do
     columns_index = 0
-    while columns_index < src[row_index].count do
-      if src[row_index][columns_index].even?
-        p src[row_index][columns_index]
+    while columns_index < aoa[row_index].count do
+      if aoa[row_index][columns_index].even?
+        p aoa[row_index][columns_index]
       end
       columns_index += 1
     end
